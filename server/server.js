ac62ba8dd90fa8d573c4aafb37d9c35b911eaad1
kevinqpham <kevin.pham@logicauto.io>
2025-03-05 14:12:33 -0600

    added volunteer history backened integration

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const authRoutes = require(path.join(__dirname, 'routes', 'authRoutes'));
+const historyRoutes = require(path.join(__dirname, 'routes', 'historyRoutes')); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/history', historyRoutes); 
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
