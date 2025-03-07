1d36dcfe758e6f7b6be2fe163bbb908fe9a0462c
alminraja <alminraja1@gmail.com>
2025-03-07 15:13:39 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,12 +3,16 @@ const cors = require('cors');
 const path = require('path');
 const authRoutes = require(path.join(__dirname, 'routes', 'authRoutes'));
 const historyRoutes = require(path.join(__dirname, 'routes', 'historyRoutes')); 
+const matchingRoutes = require(path.join(__dirname, 'routes', 'matchingRoutes'));
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/match', matchingRoutes);
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/history', historyRoutes); 
