840b8bb6b1805f8f2f142a6d423a572e302b27a5
Tom <tomxhuynh@gmail.com>
2025-03-07 14:39:54 -0600

    userProfileRoutes and notificationRoutes

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const path = require('path');
 const authRoutes = require(path.join(__dirname, 'routes', 'authRoutes'));
 const historyRoutes = require(path.join(__dirname, 'routes', 'historyRoutes'));
 const userProfileRoutes = require(path.join(__dirname, 'routes', 'userProfileRoutes'));
+const notificationRoutes = require(path.join(__dirname, 'routes', 'notificationRoutes'));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/history', historyRoutes);
 app.use('/api/profile', userProfileRoutes);
+app.use('/api/notifications', notificationRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
